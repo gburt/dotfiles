bfe5dad98812c6999fd1790960943a0708a5d3bf
Liyan David Chang <dchang@devoted.com>
2017-09-14 19:22:44 +0000

    Make sure to avoid errors when linking

--- bootstrap_work.sh
+++ bootstrap_work.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Link in the dotfiles
+# Link in the dotfiles (remove if already exists)
+rm ~/.bash_profile 2> /dev/null || true
 ln -s dotfiles/.bash_profile ~/.bash_profile
+rm ~/.gitconfig 2> /dev/null || true
 ln -s dotfiles/.gitconfig ~/.gitconfig
 
 # Have the base .vimrc source my .vimrc to avoid squashing changes
